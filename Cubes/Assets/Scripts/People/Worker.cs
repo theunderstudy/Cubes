7a9ea09d440643a5d6b094c1d6a649ee3b5c09ed
Francis <f.joseph.stephens@gmail.com>
2019-05-01 00:02:02 +1200

    worker set up

--- Cubes/Assets/Scripts/People/Worker.cs
+++ Cubes/Assets/Scripts/People/Worker.cs
@@ -1,18 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Worker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected enum WorkerStates { Nil, Idle, Wander, HarvestingResource }
+
+    private delegate void AIFunction();
+    private AIFunction CurrentAI;
+    private AIFunction NextAI;
+    #region Navigation General
+    private NavMeshAgent Agent;
+    private NavMeshHit Hit;
+    private Vector3 Destination;
+    #endregion
+    #region Wander
+    public float WanderRadius;
+    public float WanderIdleTime;
+    #endregion
+    private float currentTimer;
+    private float timer;
+    private void Awake()
     {
-        
+        Agent = GetComponent<NavMeshAgent>();
+    }
+    private Transform target;
+
+    private void Idle()
+    {
+        timer += Time.deltaTime;
+        if (timer > currentTimer)
+        {
+            CurrentAI = NextAI;
+            timer = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartWander()
     {
-        
+        if (GetLocation(out Destination))
+        {
+            Agent.destination = Destination;
+
+        }
+    }
+    private void Wander()
+    {
+        if (!Agent.hasPath)
+        {
+
+        }
+    }
+
+    private void Harvest()
+    {
+
+    }
+
+    private bool GetLocation(out Vector3 pos)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            pos = new Vector3(transform.position.x + Random.Range(0, WanderRadius), 0, (transform.position.z + Random.Range(0, WanderRadius)));
+            if (NavMesh.SamplePosition(pos, out Hit, 1.0f, NavMesh.AllAreas))
+            {
+                pos = Hit.position;
+                return true;
+            }
+        }
+        pos = Vector3.zero;
+        return false;
     }
 }
